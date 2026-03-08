c21c29722c9d79503dc7a35bdaa16ac6070704d1
beamjinn32704 <113393461+bdePIIT@users.noreply.github.com>
2026-03-08 01:51:52 -0600

    binutils

--- linux/internet_rhel.sh
+++ linux/internet_rhel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-dnf install fzf ripgrep auditd tcpdump
+dnf install fzf ripgrep auditd tcpdump strings binutils
 wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy64
 # Run av.sh?
