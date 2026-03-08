4cedaaf942fe556ae5c862892422eabfe43b3a3c
beamjinn32704 <113393461+bdePIIT@users.noreply.github.com>
2026-03-08 01:37:41 -0600

    kill_service

--- linux/kill_service.sh
+++ linux/kill_service.sh
@@ -5,12 +5,6 @@ do
 	systemctl stop "$var"
 	systemctl disable "$var"
 	systemctl mask "$var"
-	file=$(systemctl show -P FragmentPath "$var")
-	if [ -n "$file" ]; then
-		mv "$file" "$file.bak"
-		echo "$file" >> killed_services
-	else
-		echo "$var" >> killed_services
-	fi
+	echo "$var" >> killed_services
 done
 systemctl daemon-reload
