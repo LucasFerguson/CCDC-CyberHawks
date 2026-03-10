157693005dc36d5ab823bd76d1713d12083487b6
beamjinn32704 <113393461+bdePIIT@users.noreply.github.com>
2026-03-10 00:10:13 -0500

    Add policycoreutils

--- linux/internet_deb.sh
+++ linux/internet_deb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
-apt install fzf ripgrep auditd tcpdump binutils
+cd ~
+apt install fzf ripgrep auditd tcpdump binutils policycoreutils
 wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy64
 # Run av.sh?
--- linux/internet_rhel.sh
+++ linux/internet_rhel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
-dnf install fzf ripgrep auditd tcpdump strings binutils
+cd ~
+dnf install fzf ripgrep auditd tcpdump strings binutils policycoreutils
 wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy64
 # Run av.sh?
