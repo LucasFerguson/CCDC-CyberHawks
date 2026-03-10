78310c3c492b18002b0d43852aa1d0d750ecf644
beamjinn32704 <113393461+bdePIIT@users.noreply.github.com>
2026-03-10 00:24:36 -0500

    another

--- linux/internet_deb.sh
+++ linux/internet_deb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ~
-apt install fzf ripgrep auditd tcpdump binutils policycoreutils
+apt install fzf ripgrep auditd tcpdump binutils nmap
 wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy64
 # Run av.sh?
--- linux/internet_rhel.sh
+++ linux/internet_rhel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ~
-dnf install fzf ripgrep auditd tcpdump strings binutils policycoreutils
+dnf install fzf ripgrep auditd tcpdump strings binutils policycoreutils nmap
 wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy64
 # Run av.sh?
