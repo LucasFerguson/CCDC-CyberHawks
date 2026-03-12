007f38c4ff65d06cfe294a9a88fb3ad09211503a
beamjinn32704 <113393461+bdePIIT@users.noreply.github.com>
2026-03-12 17:19:46 -0500

    mallkm

---
+++ linux/kokit.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Detecting Malicious Kernel Modules
+
+# Diff /proc/modules to standard_kernel_modules file
+# lsmod | awk '{print $1}' | sort > baseline.txt
+# diff baseline.txt <(lsmod | awk '{print $1}' | sort)
+
+# Compare lsmod and /proc/modules
+
+
+# Check dmesg logs
+sudo dmesg | grep -i "module"
+
+# Check /etc/modules and /lib/modules-load.d/ for malicious .ko's
+# More generally, check systemd-modules-load.service for ConditionDirectoryNotEmpty dirs
+# and check all of those. use dropin finder functionality
+# systemd-modules-load.service will also look at the modules-load and rd.modules-load kernel command-line parameters. Find out what these are!!
+
+# Verify module signatures
+# cat /sys/module/module/parameters/sig_enforce
+# modinfo -F signer <module.ko>
+# What do above commands do?
+
+# Use rkhunter or chkrootkit
+# sudo rkhunter --check
+
+# Check for Kernel Taint!
+cat /proc/sys/kernel/tainted
+./kernel_chktaint.sh
+
+# Detect malicious kexec. This is why important to prevent reloading!
+
+# 1. Enforce Module Signing
+# Enable module signing in the kernel configuration (CONFIG_MODULE_SIG=y) to allow only trusted modules.
+
+# 2. Lock Down Module Loading
+# echo 1 > /proc/sys/kernel/modules_disabled
+
+# Restrict Access to insmod, modprobe, and /lib/modules
+# chmod 700 /sbin/insmod /sbin/modprobe
+# chown root:root /lib/modules -R
+
+# Use Mandatory Access Control
+# setsebool -P allow_kernel_modload off
+
+# Monitoring Module Events with Auditd
+# sudo auditctl -w /sbin/insmod -p x -k modload
+# sudo auditctl -w /sbin/modprobe -p x -k modload
+# ausearch -k modload
+
+# Check audit.log, kern.log, for module loading and unloading event.
+
+# What is /sys/module?
+
+# Use Volatility Framework to find hidden modules in kernel memory
