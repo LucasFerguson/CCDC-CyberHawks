697345f85bbcad69496c53859388bf1f9e97b1a3
beamjinn32704 <113393461+bdePIIT@users.noreply.github.com>
2026-03-12 16:33:04 -0500

    edit

--- linux/sokit.sh
+++ linux/sokit.sh
@@ -2,25 +2,35 @@
 # Shared Object Userland Rootkits (LD_PRELOAD/LD_LIBRARY_PATH/LD_AUDIT)
 
 # Search for bad env var sudoers entries for all users
+echo "Users with sudoers settings with LD_PRELOAD|LD_LIBRARY_PATH|LD_AUDIT:" 
 for user in $(getent passwd | cut -d: -f1); do sudo -U "$user" -l 2>/dev/null | grep -Eq "LD_PRELOAD|LD_LIBRARY_PATH|LD_AUDIT" && echo "$user"; done
 
 # Search for bad env vars in current shell
-echo "LD_PRELOAD: $LD_PRELOAD, LD_LIBRARY_PATH: $LD_LIBRARY_PATH, LD_AUDIT: $LD_AUDIT"
+echo ""
+echo "Current shell env var values -- LD_PRELOAD: $LD_PRELOAD, LD_LIBRARY_PATH: $LD_LIBRARY_PATH, LD_AUDIT: $LD_AUDIT"
 
 # Search for bad env vars in all processes
-sudo rg --text "LD_PRELOAD|LD_LIBRARY_PATH|LD_AUDIT" /proc/*/environ 2>/dev/null
+echo ""
+echo "Search for LD_PRELOAD|LD_LIBRARY_PATH|LD_AUDIT in all processes:"
+sudo rg --pre strings --text "LD_PRELOAD=|LD_LIBRARY_PATH=|LD_AUDIT=" /proc/*/environ 2>/dev/null
 
 # Search for processes using known malicious .so
-sudo rg <path_to_malicious.so> /proc/*/maps
+# sudo rg <path_to_malicious.so> /proc/*/maps
 
 # Search for .so's being used that aren't in the standard directories (/lib,/usr/lib,/lib64,/usr/lib64)
+echo ""
+echo "Shared objects being used in memory that aren't in the standard directories (/lib,/usr/lib,/lib64,/usr/lib64):"
 sudo rg "\.so" /proc/*/maps 2>/dev/null | rg -v "/usr/lib64/|/lib/|/usr/lib/|/lib64/|/usr/libexec/sudo/sudoers.so|/usr/libexec/sudo/libsudo_util.so.0.0.0"
 
 # Search places used to find shared objects
 
 # Check ld cache:
+echo ""
+echo "Check ld cache for not normal shared objects:"
 strings /etc/ld.so.cache | rg "\.so" | rg -v "/usr/lib64/|/lib/|/usr/lib/|/lib64|glibc-ld.so.cache1.1"
 
 # Check /etc/ld.so.conf
-cat /etc/ld.so.conf
-rg "." /etc/ld.so.conf.d/*
+echo ""
+echo "Print all folders checked for shared objects system-wide (found in ld.so.conf and ld.so.conf.d):"
+rg -v "^#" /etc/ld.so.conf*
+# rg "^#" /etc/ld.so.conf.d/*
