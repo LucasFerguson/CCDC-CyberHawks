a33dd1df5e3f4af62f9e702f37053902e1a08ada
beamjinn32704 <113393461+bdePIIT@users.noreply.github.com>
2026-03-12 22:43:22 -0500

    proto

--- linux/kokit.sh
+++ linux/kokit.sh
@@ -1,37 +1,43 @@
 #!/bin/bash
 # Detecting Malicious Kernel Modules
 
+sudo rkhunter -c
+sudo chkrootkit
+
 # Diff /proc/modules to standard_kernel_modules file
 # lsmod | awk '{print $1}' | sort > baseline.txt
 # diff baseline.txt <(lsmod | awk '{print $1}' | sort)
 
 # Check for out-of-tree modules
+echo "Out-of-tree (non standard kernel) loaded modules:"
 cat /proc/modules | grep "(OE)"
 
 # Compare lsmod and /proc/modules
+echo "Discrepancies between lsmod and /proc/modules:"
 lsmod | cut -d' ' -f1 | grep -Ev "^Module$" > lsmod_output
 cat /proc/modules | cut -d' ' -f1 > proc_modules_output
 diff lsmod_output proc_modules_output
 
 # Check dmesg logs
+echo "Kernel module log messages:"
 sudo dmesg | grep -i "module"
 
 # Check for autostart malicious .ko's
 # More generally, check systemd-modules-load.service for ConditionDirectoryNotEmpty dirs
-cat $(systemctl show -P FragmentPath systemd-modules-load.service) | grep "ConditionDirectoryNotEmpty=|" | cut -d'|' -f2
+echo "Check for autostart .ko's:"
+cat $(systemctl show -P FragmentPath systemd-modules-load.service) | grep "ConditionDirectoryNotEmpty=|" | cut -d'|' -f2 | while read -r line; do echo "Contents of $line:"; ls "$line" | while read -r folderfile; do echo "$line/$folderfile:"; cat "$line/$folderfile"; done; done 2>/dev/null
+
+# run modinfo on modules found.
 
 # and check all of those. use dropin finder functionality
 # systemd-modules-load.service will also look at the modules-load and rd.modules-load kernel command-line parameters. Find out what these are!!
 
 # Verify module signatures
-# cat /sys/module/module/parameters/sig_enforce
-# modinfo -F signer <module.ko>
-# What do above commands do?
-
-# Use rkhunter or chkrootkit
-# sudo rkhunter --check
+echo "Loaded modules without a signature:"
+cat /proc/modules | cut -d' ' -f1 | while read -r line; do if [ -z "$(modinfo -F signer "$line")" ]; then echo "$line"; fi; don
 
 # Check for Kernel Taint!
+echo "Check for kernel taint:"
 cat /proc/sys/kernel/tainted
 chmod +x kernel_chktaint
 ./kernel_chktaint
@@ -39,26 +45,10 @@ dmesg | grep taint
 
 # Detect malicious kexec. This is why important to prevent reloading!
 
-# 1. Enforce Module Signing
-# Enable module signing in the kernel configuration (CONFIG_MODULE_SIG=y) to allow only trusted modules.
-
-# 2. Lock Down Module Loading
-# echo 1 > /proc/sys/kernel/modules_disabled
-
-# Restrict Access to insmod, modprobe, and /lib/modules
-# chmod 700 /sbin/insmod /sbin/modprobe
-# chown root:root /lib/modules -R
-
-# Use Mandatory Access Control
-# setsebool -P allow_kernel_modload off
+# Restrict Access to insmod, modprobe
+chmod 700 "$(which insmod)" "$(which modprobe)" 
 
 # Monitoring Module Events with Auditd
-# sudo auditctl -w /sbin/insmod -p x -k modload
-# sudo auditctl -w /sbin/modprobe -p x -k modload
+sudo auditctl -w /sbin/insmod -p x -k modload
+sudo auditctl -w /sbin/modprobe -p x -k modload
 # ausearch -k modload
-
-# Check audit.log, kern.log, for module loading and unloading event.
-
-# What is /sys/module?
-
-# Use Volatility Framework to find hidden modules in kernel memory
