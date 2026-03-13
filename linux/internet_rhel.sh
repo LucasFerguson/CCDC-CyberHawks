0b94d8eb7931f8bb0760846b7133d21244548476
beamjinn32704 <113393461+bdePIIT@users.noreply.github.com>
2026-03-12 23:37:22 -0500

    chat

--- linux/internet_rhel.sh
+++ linux/internet_rhel.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd ~
 dnf install fzf ripgrep auditd tcpdump rkhunter chkrootkit dos2unix strings binutils policycoreutils nmap logrotate wget curl
 # dnf upgrade
 wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy64
