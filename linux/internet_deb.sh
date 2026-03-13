ef36edf6f4c369bea73821266724cedabf19614c
beamjinn32704 <113393461+bdePIIT@users.noreply.github.com>
2026-03-12 19:09:15 -0500

    mhm

--- linux/internet_deb.sh
+++ linux/internet_deb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ~
-apt install fzf ripgrep auditd tcpdump binutils nmap logrotate
+apt install fzf ripgrep auditd tcpdump binutils nmap logrotate wget curl
 wget https://github.com/DominicBreuker/pspy/releases/download/v1.2.1/pspy64
 # Run av.sh?
